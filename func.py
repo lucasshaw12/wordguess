4d7cc22c74945c33ea2413421042796152afc9db
Lucasshaw12 <shaw51@hotmail.co.uk>
2022-02-03 15:36:18 +0000

    Updating minor changes

--- func.py
+++ func.py
@@ -1,32 +1,39 @@
 import random
 import csv
+import sys
 
 def enter_name():
   global myName
-	# Ask the a player for their username
-  print("What is your username:")
-  myName = input()[0:6]
-  print("\n")
-  while myName.isspace() or myName.isdigit() or myName == "":
-      print("Invalid character, please use letters!\n")
-      myName = input()[0:6]
-  print("Hi, " + str(myName) + ", welcome to the Word Guessing Game!")
-  print("\n")
+  try:			
+		# Ask the a player for their username
+	  print("What is your username:")
+	  myName = input()[0:6]
+	  print("\n")
+	  while myName.isspace() or myName.isdigit() or myName == "":
+	      print("Invalid character, please use letters!\n")
+	      myName = input()[0:6]
+	  print("Hi, " + str(myName) + ", welcome to the Word Guessing Game!")
+	  print("\n")
+  except KeyboardInterrupt:
+	  sys.exit()  
 
 def retry_game():
-	# Ask the player whether they want to retry
-	# Choose a word from the list at random
-	tryAgain = str(input("Try again: (Y = yes / N = no)"))
-	tryAgain = tryAgain.lower()
-	if tryAgain == "y" or tryAgain == "yes":
-	  from wordguess import main
-	elif tryAgain == "n" or tryAgain == "no":
-	  exit()
-	  print("else")
-	else:
-	  print("\n")
-	  print("Invalid input. Please use (Y = yes / N = no)")
-	  print("\n")
+ try:	
+		# Ask the player whether they want to retry
+		# Choose a word from the list at random
+		tryAgain = str(input("Try again: (Y = yes / N = no)"))
+		tryAgain = tryAgain.lower()
+		if tryAgain == "y" or tryAgain == "yes":
+		  from wordguess import main
+		elif tryAgain == "n" or tryAgain == "no":
+		  exit()
+		  print("else")
+		else:
+		  print("\n")
+		  print("Invalid input. Please use (Y = yes / N = no)")
+		  print("\n")
+ except KeyboardInterrupt:
+  	sys.exit()
 
 def choose_word():
 	# Create a list of hangman words
--- wordguess.py
+++ wordguess.py
@@ -2,8 +2,8 @@ def main():
   import random
   import datetime, time
   import sys
-  import csv
-  import keyboard
+  import csv   
+  #import pyautogui
 
   # Bespoke function that holds all custom functions for the game
   import func
@@ -38,92 +38,97 @@ INSTRUCTIONS:\n
   print(" ".join(display))
 
   func.enter_name()
+  try:
+    # Keep asking the player until all letters are guessed
+    while display != wordChosen:
+      guess = input(str("Please enter a guess for the {} ".format(len(display)) + "letter word: "))[0:1] # [0:1] ensure that the inout only uses the 1st letter
+      if guess.isspace() or guess.isdigit() or guess == "":
+        print("Invalid character, please use a single letter\n")
+        continue
+      guess = guess.lower()
+      #Add the players guess to the list of used letters
+      used.extend(guess)
+      print("Attempts: ")
+      print(attempts)
 
-  # Keep asking the player until all letters are guessed
-  while display != wordChosen:
-    guess = input(str("Please enter a guess for the {} ".format(len(display)) + "letter word: "))[0:1]
-    if guess.isspace() or guess.isdigit() or guess == "":
-      print("Invalid character, please use a single letter\n")
-      continue
-    guess = guess.lower()
-    #Add the players guess to the list of used letters
-    used.extend(guess)
-    print("Attempts: ")
-    print(attempts)
+      # The below 3 lines closes the program when 'esc' is typed as a guess. '[0:1]' prevents this from working
+      # if guess == 'esc':
+      #   print('exiting.....')
+      #   sys.exit(0)
 
-    print(wordChosen) # Added for testing ///////////////////////////////////////////////////////////
+      print(wordChosen) # Added for testing ///////////////////////////////////////////////////////////
 
-    # Provide a hint if unsuccessful after 7 attempts
-    if attempts >= 10 and guess != wordChosen and wordChosen in wordList[0:6]:
-      print("HINT: It's an animal")
-    elif attempts >= 10 and guess != wordChosen and wordChosen in wordList[7:11]:
-      print("HINT: It's a vehicle!")
-    elif attempts >= 10 and guess != wordChosen and wordChosen in wordList[11:14]:
-      print("HINT: It's food!")  
+      # Provide a hint if unsuccessful after 7 attempts
+      if attempts >= 10 and guess != wordChosen and wordChosen in wordList[0:6]:
+        print("HINT: It's an animal")
+      elif attempts >= 10 and guess != wordChosen and wordChosen in wordList[7:11]:
+        print("HINT: It's a vehicle!")
+      elif attempts >= 10 and guess != wordChosen and wordChosen in wordList[11:14]:
+        print("HINT: It's food!")  
 
-      # # Allow the player to exit the game, 
-      # # Currently uses temrinal function of ctrl + c
-      # if keyboard.read_key() == 'p':
-      #   print("Exiting...")
-      #   sys.exit(0) # this exits your program with exit code 0
-      # else:
-      #   break
+        # # Allow the player to exit the game, 
+        # # Currently uses terminal function of ctrl + c
+        # if guess.read_key() == 'p':
+        #   print("Exiting...")
+        #   sys.exit(0) # this exits your program with exit code 0
+        # else:
+        #   break
 
-    # Search through the letters in answer
-    for i in range(len(wordChosen)):
-      if wordChosen[i] == guess:
-        display = display[0:i] + guess + display[i+1:]
+      # Search through the letters in answer
+      for i in range(len(wordChosen)):
+        if wordChosen[i] == guess:
+          display = display[0:i] + guess + display[i+1:]
 
-    print("Used letters: ")
-    print(used)
-        
-    # Print the string with guessed letters (with spaces in between))
-    print(" ".join(display))
+      print("Used letters: ")
+      print(used)
+          
+      # Print the string with guessed letters (with spaces in between))
+      print(" ".join(display))
 
-    # Create a space for formatting  
-    print("\n")
+      # Create a space for formatting  
+      print("\n")
 
-    # Add +1 to attempts after each guess
-    attempts = attempts + 1
+      # Add +1 to attempts after each guess
+      attempts = attempts + 1
 
-    # Capture date and time of successful attempt
-    if display == wordChosen:
-      dt = datetime.datetime.now()
-      dtFullDate = dt.day, dt.month, dt.year
-      print('\n')
+      # Capture date and time of successful attempt
+      if display == wordChosen:
+        dt = datetime.datetime.now()
+        dtFullDate = dt.day, dt.month, dt.year
+        print('\n')
 
-      # When successful, add the username to the scoreboard.csv file list and display (Excel)
-      # Run a function that displays the scoreboard 
-      # Add the round to the scoreboard
-      with open('scoreboard.csv', 'a', newline='') as outputFile:
-        outputWriter = csv.DictWriter(outputFile, ['Name', 'Date', 'Attempts', 'Word Chosen'])
-        outputWriter.writerow({'Name' : func.myName, 'Date' : dtFullDate, 'Attempts' : attempts, 'Word Chosen' : wordChosen})
-        outputFile.close()
+        # When successful, add the username to the scoreboard.csv file list and display (Excel)
+        # Run a function that displays the scoreboard 
+        # Add the round to the scoreboard
+        with open('scoreboard.csv', 'a', newline='') as outputFile:
+          outputWriter = csv.DictWriter(outputFile, ['Name', 'Date', 'Attempts', 'Word Chosen'])
+          outputWriter.writerow({'Name' : func.myName, 'Date' : dtFullDate, 'Attempts' : attempts, 'Word Chosen' : wordChosen})
+          outputFile.close()
 
-        # Display the scoreboard from the scoreboard.csv file
-        print(("************* scoreboard *************\n".upper()))
-        print("""Name     Date       Atmpt Word""")
-        outputFile = open('scoreboard.csv')
-        outputDictReader = csv.DictReader(outputFile, ['Name', 'Date', 'Attempts', 'Word Chosen'])
-        for row in outputDictReader:
-          print(row['Name'], row['Date'], row['Attempts'], row['Word Chosen'])
-        outputFile.close()
+          # Display the scoreboard from the scoreboard.csv file
+          print(("************* scoreboard *************\n".upper()))
+          print("""Name     Date       Atmpt Word""")
+          outputFile = open('scoreboard.csv')
+          outputDictReader = csv.DictReader(outputFile, ['Name', 'Date', 'Attempts', 'Word Chosen'])
+          for row in outputDictReader:
+            print(row['Name'], row['Date'], row['Attempts'], row['Word Chosen'])
+          outputFile.close()
 
-  print("Well done " + func.myName.capitalize() + ", you guessed '" + str(wordChosen) + "' right in " + str(attempts) + " attempts!")
+    print("Well done " + func.myName.capitalize() + ", you guessed '" + str(wordChosen) + "' right in " + str(attempts) + " attempts!")
 
-  # Ask the player whether they want to retry
-  while display == wordChosen:
-    func.retry_game()
+    # Ask the player whether they want to retry
+    while display == wordChosen:
+      func.retry_game()
+
+  except KeyboardInterrupt:
+    sys.exit()
 
 main()
 
 
 # TO DO -------------
-# add a quit function,
-# Add a header to the scoreboard.csv file without it writing after each gameround
+# add a quit function using 'ESC' whilst still restricting ke
 # for the name input, ensure that a minimum of 3 characters are provided and only letters
-# Refactor code = add classes, functions etc - enterName(), retry_game(), choose_word() 
-
 
 # Done list -----------
 # Basic game function
@@ -137,3 +142,5 @@ main()
 # fix bug if 'enter' and 'esc' is pressed on the guess stage then error occurs 
 # Ensure no numbers are allowed within input() when guessing a word
 # Use a dictionary of words which will imported from a .csv
+# Refactor code = add classes, functions etc - enterName(), retry_game(), choose_word()
+# Add a header to the scoreboard.csv file without it writing after each gameround
